e56681c9a2b6c363d91167e60aaa2e88c5417bf5
Aditya Gupta <adityagupta1089@gmail.com>
2019-04-26 02:48:29 +0530

    Check End Ride

--- src/main/java/com/csl456/bikerentalapp/resources/RideResource.java
+++ src/main/java/com/csl456/bikerentalapp/resources/RideResource.java
@@ -14,7 +14,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Date;
 import java.util.List;
 
@@ -50,6 +52,11 @@ public class RideResource {
     public Ride endRide(@FormParam("endLocationId") int endLocationId,
             @FormParam("rideId") int rideId) {
         Ride ride = rideDAO.getById(rideId);
+        if (ride.getEndTime() != null) {
+            throw new WebApplicationException("Ride Already Ended",
+                    Response.Status.FORBIDDEN
+            );
+        }
         ride.setEndLocationId(endLocationId);
         ride.setEndTime(new Date());
         ride.calculateCost();
