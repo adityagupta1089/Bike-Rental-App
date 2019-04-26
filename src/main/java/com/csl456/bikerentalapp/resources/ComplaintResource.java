e7e106ac7a62124ade57ee7e531cc72d043389b3
Aditya Gupta <adityagupta1089@gmail.com>
2019-04-26 05:44:07 +0530

    Change Resolve Complaint

--- src/main/java/com/csl456/bikerentalapp/resources/ComplaintResource.java
+++ src/main/java/com/csl456/bikerentalapp/resources/ComplaintResource.java
@@ -10,10 +10,10 @@ import io.dropwizard.hibernate.UnitOfWork;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -49,9 +49,9 @@ public class ComplaintResource {
     @DELETE
     @UnitOfWork
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("{id}")
     @RolesAllowed(UserRole.ADMIN)
-    public Complaint resolveComplaint(
-            @FormParam("complaintId") int complaintId) {
+    public Complaint resolveComplaint(@PathParam("id") int complaintId) {
         Complaint complaint = complaintDAO
                 .getById(complaintId)
                 .orElseThrow(() -> new WebApplicationException(
